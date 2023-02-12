33a624a2cc30e17a328f4ba30470ac3cc22b7e38
tsunodamakoto <tunoda2227@gmail.com>
2023-02-12 15:32:34 +0900

    f

--- js/common.js
+++ js/common.js
@@ -6,19 +6,6 @@ $(window).on('load',function(){
 		$("#header-nav").fadeToggle(200);
 		$("#header-bg").fadeToggle(200);
         // スクロール禁止
-        var moveFun = function(event) {
-            event.preventDefault();
-        }
-        if($(".act").length) { 
-            window.addEventListener('wheel', moveFun, { passive: false });
-        } else {
-            window.removeEventListener('wheel', moveFun, { passive: false });
-        }
-        if($(".act").length) { 
-            window.addEventListener('touchmove', moveFun, { passive: false })
-          } else {
-            window.removeEventListener('touchmove', moveFun, { passive: false });
-          }
         if($(".act").length) { 
             $("body").css('overflow','hidden');
         } else {

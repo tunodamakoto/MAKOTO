8953feb3ffc4ed9cd3a9be882c0b7e5b04877a66
tsunodamakoto <tunoda2227@gmail.com>
2023-02-13 15:31:19 +0900

    n

--- js/common.js
+++ js/common.js
@@ -27,3 +27,54 @@ if($('body#top').length){
         pagerCustom: '.bx-pager'
     });
 };
+
+// Use [about]
+if($('body#about').length){
+    $(window).on('scroll', function() {
+        var chart = $('.chart');
+        chart.each(function() {
+            var chartOffset = $(this).offset().top;
+            var scrollPos = $(window).scrollTop();
+            var wh = $(window).height();
+            if(scrollPos > chartOffset - wh + (wh / 2)){
+                if (window.matchMedia('(min-width: 769px)').matches) {
+                    $('.chart').easyPieChart({
+                        easing: 'easeOutQuart',
+                        barColor: '#666666',
+                        trackColor: '#CDCDCD',
+                        animate: 5000,
+                        lineWidth: 8,
+                        size: 160,
+                        trackWidth: 6,
+                        scaleLength: 10,
+                        scaleColor: false,
+                    });
+                } else if (window.matchMedia('(max-width: 768px) and (min-width: 521px)').matches) {
+                    $('.chart').easyPieChart({
+                        easing: 'easeOutQuart',
+                        barColor: '#666666',
+                        trackColor: '#CDCDCD',
+                        animate: 5000,
+                        lineWidth: 8,
+                        size: 130,
+                        trackWidth: 6,
+                        scaleLength: 10,
+                        scaleColor: false,
+                    });
+                } else if (window.matchMedia('(max-width:520px)').matches) {
+                    $('.chart').easyPieChart({
+                        easing: 'easeOutQuart',
+                        barColor: '#666666',
+                        trackColor: '#CDCDCD',
+                        animate: 5000,
+                        lineWidth: 8,
+                        size: 110,
+                        trackWidth: 6,
+                        scaleLength: 10,
+                        scaleColor: false,
+                    });
+                }
+            }
+        });
+    });
+};
